902300441979e7500b8a95df4b9eb6167b533dec
Jeremy Gaines <jeremy.gaines@outlook.com>
2024-02-13 23:28:18 -0800

    Added snake movements. Need to work on caching key presses.

--- Player.cs
+++ Player.cs
@@ -5,8 +5,15 @@ public partial class Player : Area2D
 {
 	
 	[Export]
-	public int Speed { get; set; } = 400;
+	public int Speed { get; set; } = 100;
+	
+	[Export]
+	public float RotationSpeed { get; set; } = 90 * MathF.PI / 180 ;
+	
 	public Vector2 ScreenSize;
+	public Vector2 Velocity = Vector2.Zero;
+	
+	private float _rotationDirection;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -17,6 +24,22 @@ public partial class Player : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+			
+		// Get snake direction
+		_rotationDirection = Input.GetAxis("turn_left", "turn_right");
+		
+		Rotation += _rotationDirection * RotationSpeed;
+		
+		//Apply Snake rotation
+		Velocity.X = Speed * MathF.Cos(Rotation);
+		Velocity.Y = Speed * MathF.Sin(Rotation);
+		
+		// Player movement
+		Position += Velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
 	}
 
 	
