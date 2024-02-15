35389dc8f38c0bafdfa203a835f89a46c872cbe6
Jeremy Gaines <jeremy.gaines@outlook.com>
2024-02-14 23:30:53 -0800

    Updated snake head behaviour

--- Player.cs
+++ Player.cs
@@ -5,41 +5,48 @@ public partial class Player : Area2D
 {
 	
 	[Export]
-	public int Speed { get; set; } = 100;
-	
-	[Export]
-	public float RotationSpeed { get; set; } = 90 * MathF.PI / 180 ;
-	
+	public int Speed { get; set; } = 100;	
 	public Vector2 ScreenSize;
 	public Vector2 Velocity = Vector2.Zero;
 	
-	private float _rotationDirection;
+	private float _rotationAngle { get; set; } = 90 * MathF.PI / 180 ;
+	private float _turnDirection;
+	private float _turnQueau;
+	
+	// Input control
+	public override void _Input(InputEvent @event)
+	{		
+		if(@event.IsPressed())
+		{
+			_turnQueau += Input.GetAxis("turn_left", "turn_right");
+		}
+		if(@event.IsReleased())
+		{
+			_turnDirection = _turnQueau;
+		}
+	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ScreenSize = GetViewportRect().Size;	
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-			
-		// Get snake direction
-		_rotationDirection = Input.GetAxis("turn_left", "turn_right");
-		
-		Rotation += _rotationDirection * RotationSpeed;
-		
-		//Apply Snake rotation
+		// Apply snake direction		
+		Rotation = _turnDirection * _rotationAngle;
 		Velocity.X = Speed * MathF.Cos(Rotation);
 		Velocity.Y = Speed * MathF.Sin(Rotation);
 		
-		// Player movement
+		// Snake movement
 		Position += Velocity * (float)delta;
 		Position = new Vector2(
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
+		
 	}
 
 	
